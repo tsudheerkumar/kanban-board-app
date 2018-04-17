b15abcaa76fa1940dbe68b5ea1371ac45fe51341
Sudheer Kumar Tumarada <stumarada@sapient.com>
2018-04-17 18:43:20 +0530

    Added actions i reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,32 @@
-export default(state = [], payload) => {
+export default (state = [], payload) => {
     switch (payload.type) {
         case 'add_task':
             return [...state, payload.item];
+
+        case 'delete_task':
+            return [...state, payload.item];
+
+        case 'update_task':
+            return [...state, payload.item];
+
+        case 'add_list':
+            return [...state, payload.item];
+
+        case 'delete_list':
+            return [...state, payload.item];
+
+        case 'update_list':
+            return [...state, payload.item];
+
+        case 'add_board':
+            return [...state, payload.item];
+
+        case 'update_board':
+            return [...state, payload.item];
             
+        case 'delete_board':
+            return [...state, payload.item];
+
         default:
             return state;
     }
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import rootReducer from  './reducers';
+import rootReducer from  './reducers/index';
 export default(initialState) => {
     return createStore(rootReducer, initialState);
 }
